cf710be36b681fde4ae1372ac2b1912e1db2b773
Kaisa Ets <kaisaets@gmail.com>
2026-02-19 18:40:35 +0200

    connection to database

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  "mysql://root:qwerty@localhost:3306/yoga_sequelize",
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database: ", err);
+  });
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to sequelize application." });
 });
